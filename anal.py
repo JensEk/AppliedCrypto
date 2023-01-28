a4c53ebb375671f0e63895846ab95788c9ada05f
Jens Ekenblad <jensekenblad@hotmail.com>
2023-01-28 17:55:57 +0100

    Vigenere solution added

--- anal.py
+++ anal.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
 
+
+# Progam to analyse and decrypt ceasar, substitution and vigenere ciphers.
+# Made by: Jens Ekenblad
+# Date: 2022-01-26
+
+import math
+
 alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_#"     # 38 characters in total
+frequencyEng = {"A":"0.072", "B":"0.013", "C":"0.024", "D":"0.037", "E":"0.112", "F":"0.020", "G":"0.018", "H":"0.054", "I":"0.061", "J":"0.001", "K":"0.007", "L":"0.035", "M":"0.021", "N":"0.059", "O":"0.066", "P":"0.017", "Q":"0.001", "R":"0.053", "S":"0.056", "T":"0.080", "U":"0.024", "V":"0.009", "W":"0.021", "X":"0.001", "Y":"0.017", "Z":"0.001", "_":"0.120", }
+
+
+# Solution for c1.txt
+solution1 = [('Y', '|'), ('S', 'e'), ('K', 'a'), ('_', 'i'), ('6', 'm'), ('R', 'n'), ('7', 'd'), ('1', 't'), ('F', 'h'), ('Z', 'r'), ('N', 'l'), ('Q', 'w'), ('A', 'o'), ('B', 'f'), ('W', 'g'), ('M', 'u'), ('5', 'v'), ('I', 's'), ('3', 'b'), ('U', 'y'), ('O', 'c'), ('D', 'z'), ('9', 'x'), ('V', 'p'), ('E', 'q'), ('J', 'j'), ('2', 'k'), ('L', '1'), ('4', '2'), ('P', '3'), ('8', '4'), ('T', '5'), ('C', '6'), ('X', '7'), ('G', '8'), ('#', '9'), ('H', '#')]
+
 
 
 # frequency analysis
-def analyseLetters(readLetters):
+def analyseLetters(cipherText):
+    
+    readLetters = {}
+    for letter in cipherText:
+            if letter in readLetters:
+                readLetters[letter] += 1
+            else:
+                readLetters[letter] = 1
+    
     sortedLetters = sorted(readLetters.items(), key= lambda x:x[1], reverse=True)        
     print("Lettercount: ", sortedLetters)
     for letter in sortedLetters:
@@ -12,14 +33,37 @@ def analyseLetters(readLetters):
     print()
     return sortedLetters[0:2]
 
-# Manuell bruteforce for most common letter in english "e" and "_"
-# encryption = (plaintext + key) % 38
-# Y(34) = ( _(36) + -2 || 36 ) % 38
-# S(28) = ( _(36) + -8 || 30 ) % 38
 
-# Y(34) = ( e(14) + -18 || 20 ) % 38
-# S(28) = ( e(14) + -24 || 14 ) % 38
+# column frequency analysis for Vigenere where each column is stepped by the keylength
+def analyseColumns(columns):
+    
+    allCol = []
+    underscore = []
+    for col in columns:
+        
+        readLetters = {}
+        for letter in col:
+                if letter in readLetters:
+                    readLetters[letter] += 1
+                else:
+                    readLetters[letter] = 1
+        
+        set = {}
+        sortedLetters = sorted(readLetters.items(), key= lambda x:x[1], reverse=True)        
+        for letter in sortedLetters:
+            set[letter[0]] = round(letter[1]/sum(readLetters.values()), 5)
+        allCol.append(set)        
+        
+    for set in allCol:
+        underscore.append(list(set.keys())[0])
+        print(f"Freq of {allCol.index(set)} : {set}", end="\n\n")  
+    
+    return underscore
+ 
 
+        
+
+# Brute shifting the ceasar cipher
 def bruteCeasar(cipher):
     print(f"Bruteforcing ceasar cipher: {cipher}")
     for key in range(1, len(alphabet)):
@@ -28,7 +72,9 @@ def bruteCeasar(cipher):
             shiftedWord += alphabet[((alphabet.find(c) - key)%len(alphabet))]
        
         print(f"Cipher shifted {key} times:", shiftedWord)
-    print()  
+    print() 
+    return shiftedWord 
+
 
 # Try swapping the most common letter with "e" and "|" to represent _ and see if it makes sense
 def analyseSubstitution(cipher, topletters):
@@ -50,7 +96,9 @@ def analyseSubstitution(cipher, topletters):
     
     readWords = analyzeWords(swapCipher)
     return (swapCipher, readWords)
-    
+ 
+
+# Extract each individual word and count the occurence of each word along with the occurence of common doubles like "dd"    
 def analyzeWords(swapCipher):
     
     readWords = {}
@@ -70,11 +118,14 @@ def analyzeWords(swapCipher):
                     
     readWords = sorted(readWords.items(), key= lambda x:(len(x[0]), x[1]), reverse=True)
     for word in readWords:
-        print(str(word).replace("(", "").replace(")", ""))
+        if word[1] > 2:
+            print(str(word).replace("(", "").replace(")", ""))
     print()
     print("Common doubles: ", commonDoubles)
     return readWords
 
+
+# Manueally swap letters to see if it makes sense and keep the history of swaps
 def swapLetter(swapCipher, readWords):
     
     cont = "y"
@@ -108,46 +159,173 @@ def swapLetter(swapCipher, readWords):
         
         cont = input("Continue? (y/n): ")
         if cont == "n":
-            return copyCipher.replace("|", " ").replace("\n").upper()
+            return copyCipher.replace("|", " ").replace("#", "\n").upper()
 
+
+
+# Split cipher into sets of size 2-4 and calculate the gcd of the distances between each occurence of the nGram
+def vigenereAnalyse(cipher):
+    
+    nGram = {}
+    gcdGlobal = {}
+    
+    for n in range(2, 5):    
+        # Split cipher into nGrams and store the position of each nGram
+        for i in range(0, len(cipher)-(n-1)):
+            if cipher[i:i+n] in nGram:
+                nGram[cipher[i:i+n]].append(i)
+            else:
+                nGram[cipher[i:i+n]] = [i]
+        
+        # If nGram occurs more than once, calculate the distance between each occurence and gdc    
+        for j in nGram:
+            if len(nGram[j]) > 1:
+                dst = []
+                gcd = []
+                for i in range(len(nGram[j])-1):
+                    dst.append(nGram[j][i+1] - nGram[j][i])
+                    if i > 0:
+                        gcd.append(math.gcd(dst[i], dst[i-1]))
+            
+                #print(f"nGram: {j}:   pos={nGram[j]}    Dst={dst}   GCD={gcd}")
+                for g in gcd:
+                    if g in gcdGlobal:
+                        gcdGlobal[g] += 1
+                    else:
+                        gcdGlobal[g] = 1
+             
+    gcdGlobal = sorted(gcdGlobal.items(), key= lambda x:(x[1]), reverse=True)            
+    print(f"Most common GCD over all [2-4]Grams (TYPE, AMOUNT): {gcdGlobal[0], gcdGlobal[1]}")
+    
+    return gcdGlobal[0][0]     
+
+
+
+def vigenereCrack(cipher, gcd):
+    
+    gcdSplit = []
+    print("Splitting cipher into columns based on GCD...", "\n")
+    for i in range(gcd):
+        gcdSplit.append(cipher[i::gcd])
+    
+    
+    ciphMod = ""
+    plain = ""
+    history = [""]*len(gcdSplit)
+    c = "y"
+    while c == "y":
+            
+        for column in gcdSplit:
+            print(f"Key {gcdSplit.index(column)} analysed: {column[0:150]}")
+        
+        print()
+        print("English frequency: ", sorted(frequencyEng.items(), key= lambda x:(x[1]), reverse=True), "\n")
+        mostCommonLetter = analyseColumns(gcdSplit)
+        
+        a = input("SwapUnderscore / PrintCipher / SwapSingel / bruteKey / End? (u / p / s / k / e): ")
+        print()
+        if a == "u":
+            for i in range(len(gcdSplit)):
+                gcdSplit[i] = gcdSplit[i].replace(mostCommonLetter[i], "|")
+                history[i] +=  (f"{mostCommonLetter[i]} -> |, ")
+        elif a == "p":
+            for i in range(len(gcdSplit[0])):
+                for j in range(len(gcdSplit)):
+                    ciphMod += gcdSplit[j][i]
+                    print(gcdSplit[j][i], end="")
+            print()
+        elif a == "s":
+            col = int(input("Enter column number to swap letters: "))
+            print(f"Histogram of column:  {history[col]}:", "\n\n")
+            x,y = input("Enter letters to swap X -> y: ").split()
+            if x != y:
+                gcdSplit[col] = gcdSplit[col].replace(x, y)
+                history[col] +=  (f"{x} -> {y}, ")
+        elif a == "k":
+            ciph, plain = input(f"Enter cipher and possible plain of length {gcd}: ").split()
+            key = keyBruteVigenere(ciph, plain)
+            plain = decryptVigenere(cipher[0:500], key)
+            if plain != "":
+                return plain
+        elif a == "e":
+            c = "n"
+            return plain
+        
+        print()
+        
+        
+        
+    
+
+def decryptVigenere(cipher, key):
+    
+    plain = ""
+    for i in range(len(cipher)):
+        plain += alphabet[(alphabet.index(cipher[i]) - alphabet.index(key[i%len(key)]))%len(alphabet)]
+    
+    print("Possible Plain: ", plain, "\n\n")
+    i = input("Correct decryption? (y/n): ")
+    if i == "y":
+        return plain
+    else:
+        return ""
+    
+def keyBruteVigenere(cipher, plain):
+ 
+    key = ""
+    for i in range(len(cipher)):
+        key += alphabet[(alphabet.index(cipher[i]) - alphabet.index(plain[i]))%len(alphabet)]
+    print("Possible Key: ", key, "\n\n")
+    
+    return key
+
+
+# Hardcoded solution once key is found
+def hardcodedSolution(cipher):
+   
+    for set in solution1:
+        cipher = cipher.replace(set[0], set[1])
+
+    return cipher.replace("|", " ").replace("#", "\n").upper()
+    
+# Main function to read input file and to write output to file
 def main():
-    readLetters = {}
+
     cipherText = ""
+    finalCipher = ""
     
-    textFile = input("Enter crypto filename: ")
-    
-    with open(textFile, "r", encoding='UTF-8') as file:
+    inputFile = input("Enter crypto file to decrypt: ")
+    with open(inputFile, "r", encoding='UTF-8') as file:
         for line in file:
             if line[-1] == '\n':
                 cipherText += line[:-1]
             else:
                 cipherText += line
-
-        for letter in cipherText:
-            if letter in readLetters:
-                readLetters[letter] += 1
-            else:
-                readLetters[letter] = 1
     file.close()        
-    mostCommon = analyseLetters(readLetters)
     
-    cipherType = input("Ceasar or substitution? (c/s): ")
+    cipherType = input("Ceasar / Substitution / Vigenere / HardcodedSolution? (c / s / v / h): ")
+    
     if cipherType == "c":
-        bruteCeasar(cipherText[0:70])
-    else:
+        finalCipher = bruteCeasar(cipherText[0:70])
+    elif cipherType == "s":
+        mostCommon = analyseLetters(cipherText)
         swappedCipher, readWords = analyseSubstitution(cipherText, mostCommon)
         finalCipher = swapLetter (swappedCipher, readWords)
-
+    elif cipherType == "v":
+        gcd = vigenereAnalyse(cipherText)
+        finalCipher =vigenereCrack(cipherText, gcd)
+    elif cipherType == "h":
+        finalCipher = hardcodedSolution(cipherText)
+ 
     print(finalCipher)
     wrt = input("Write to file? (y/n): ")
     if wrt == "y":
-        with open("decrypted.txt", "w") as outfile:
+        name = input("Enter filename: ")
+        with open(name, "w") as outfile:
             outfile.writelines(finalCipher)
             outfile.close()
-    
-    
-    
-    solution = [('K', 'a'), ('_', 'i'), ('6', 'm'), ('R', 'n'), ('7', 'd'), ('1', 't'), ('F', 'h'), ('Z', 'r'), ('N', 'l'), ('Q', 'w'), ('A', 'o'), ('B', 'f'), ('W', 'g'), ('M', 'u'), ('5', 'v'), ('I', 's'), ('3', 'b'), ('U', 'y'), ('O', 'c'), ('D', 'z'), ('9', 'x'), ('V', 'p'), ('E', 'q'), ('J', 'j'), ('2', 'k'), ('L', '1'), ('4', '2'), ('P', '3'), ('8', '4'), ('T', '5'), ('C', '6'), ('X', '7'), ('G', '8'), ('#', '9'), ('H', '#')]
+
+
 
 if __name__ == "__main__":
     main()
